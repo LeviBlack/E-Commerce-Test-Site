ea4a71f9cd5bb977320e6a3d476ddd6b9fff6a10
LeviBlack <levi.t.black.lb@gmail.com>
2021-09-22 10:22:01 -0700

    Creating review form and connecting back button to previous state.

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,12 +1,12 @@
 import React, {useState, useEffect} from 'react'
 import { ImportLabel, Select, MenuItem, Button, Grid, Typography, InputLabel } from '@material-ui/core'
 import { useForm, FormProvider } from 'react-hook-form'
-
+import { Link } from 'react-router-dom'
 import { commerce } from '../../lib/commerce'
 
 import FormInput from './CustomTextField'
 
-const AddressForm = ({ checkoutToken}) => {
+const AddressForm = ({ checkoutToken, next }) => {
     const [shippingCountries, setShippingCountries] = useState([])
     const [shippingCountry, setShippingCountry] = useState('')
     const [shippingSubdivisions, setShippingSubdivisions] = useState([])
@@ -57,7 +57,7 @@ const AddressForm = ({ checkoutToken}) => {
         <>
             <Typography variant='h6' gutterBottom>Shipping Address</Typography>
             <FormProvider {...methods}>
-                <form onSubmit=''>
+                <form onSubmit={methods.handleSubmit((data) => next({ ...data.shippingCountry, shippingSubdivision, shippingOption }))}>
                     <Grid container spacing={3}>
                         <FormInput name='firstName' label='First Name'/>
                         <FormInput name='lastName' label='Last Name'/>
@@ -96,6 +96,11 @@ const AddressForm = ({ checkoutToken}) => {
                             </Select>
                         </Grid>
                     </Grid>
+                    <br />
+                        <div style={{ display: 'flex', justifyContent: 'space-between'}}>
+                            <Button component={Link} to='/cart' variant='outlined'>Back to Cart</Button>
+                            <Button type='submit' variant='contained' color='primary' >Next</Button>
+                        </div>
                 </form>
             </FormProvider>
         </>
